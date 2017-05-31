c7665dcee63b99faa8d960a8e609a25b77dbe98f
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-05-31 12:33:33 +0200

    worked on log utils

--- bx_deploy_marmotta.sh
+++ bx_deploy_marmotta.sh
@@ -8,7 +8,7 @@ docker push nimbleplatform/marmotta-backend
 bx ic cpi nimbleplatform/marmotta-backend registry.eu-gb.bluemix.net/semantic_mediator_container/marmotta-backend
 
 # run postgres container
-bx ic run --name marmotta-db -e "POSTGRES_PASSWORD=changeme" -e "POSTGRES_USER=root" -e "POSTGRES_DB=marmotta" -p 5432:5432  -m 128 registry.eu-gb.bluemix.net/semantic_mediator_container/postgre
+bx ic run --name marmotta-db -e "POSTGRES_PASSWORD=changeme" -e "POSTGRES_USER=root" -e "POSTGRES_DB=marmotta" -p 5432:5432  -m 256 registry.eu-gb.bluemix.net/semantic_mediator_container/postgre
 
 # run marmotta
-bx ic run --name marmotta-backend --link marmotta-db:marmotta-db -p 8080:8080 -m 256 registry.eu-gb.bluemix.net/semantic_mediator_container/marmotta-backend
+bx ic run --name marmotta-backend --link marmotta-db:marmotta-db -p 8080:8080 -m 1024 registry.eu-gb.bluemix.net/semantic_mediator_container/marmotta-backend
--- run.sh
+++ run.sh
@@ -25,19 +25,23 @@ elif [[ "$1" = "start" ]]; then
 	update_images
 
 	# start infrastructure
-	docker-compose -f infra/docker-compose.yml -f infra/uaa/docker-compose.yml --project-name nimbleinfra up -d --build --force-recreate
+	docker-compose -f infra/docker-compose.yml -f infra/uaa/docker-compose.yml --project-name nimbleinfra up -d --build
 
 	# wait for gateway proxy (last service started before)
 	echo "Stalling for Gateway Proxy"
 	docker run --rm --net=nimbleinfra_default -it mcandre/docker-wget --retry-connrefused --waitretry=5 --read-timeout=20 --timeout=15 --tries 60 gateway-proxy:80
 
 	# start services
-	docker-compose -f services/docker-compose.yml --project-name nimbleservices up --build --force-recreate
+	docker-compose -f services/docker-compose.yml --project-name nimbleservices up -d --build
 
 elif [[ "$1" = "stop" ]]; then
 	
 	docker-compose -f services/docker-compose.yml --project-name nimbleservices down
 	docker-compose -f infra/docker-compose.yml -f infra/uaa/docker-compose.yml --project-name nimbleinfra down
+
+elif [[ "$1" = "services-logs" ]]; then
+
+	docker-compose -f services/docker-compose.yml --project-name nimbleservices logs -f
 	
 else
     echo Usage: $0 COMMAND
