a7055a2f7f157220d84bcb066931dc0ef75d4053
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-06-06 23:54:39 +0200

    added more resources to marmotta on Bluemix

--- bx_deploy_marmotta.sh
+++ bx_deploy_marmotta.sh
@@ -11,4 +11,4 @@ bx ic cpi nimbleplatform/marmotta-backend registry.eu-gb.bluemix.net/semantic_me
 bx ic run --name marmotta-db -e "POSTGRES_PASSWORD=changeme" -e "POSTGRES_USER=root" -e "POSTGRES_DB=marmotta" -p 5432:5432  -m 256 registry.eu-gb.bluemix.net/semantic_mediator_container/postgre
 
 # run marmotta
-bx ic run --name marmotta-backend --link marmotta-db:marmotta-db -p 8080:8080 -m 1024 registry.eu-gb.bluemix.net/semantic_mediator_container/marmotta-backend
+bx ic run --name marmotta-backend -v /var/lib/marmotta --link marmotta-db:marmotta-db -p 8080:8080 -m 2048 registry.eu-gb.bluemix.net/semantic_mediator_container/marmotta-backend
