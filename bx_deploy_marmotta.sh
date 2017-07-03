044e2e5c20be6774da70b3947765361577e088c6
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-07-03 14:36:33 +0200

    worked on shared volume for IBM container

--- bx_deploy_marmotta.sh
+++ bx_deploy_marmotta.sh
@@ -7,8 +7,21 @@ docker push nimbleplatform/marmotta-backend
 # add to Bluemix container registry
 bx ic cpi nimbleplatform/marmotta-backend registry.eu-gb.bluemix.net/semantic_mediator_container/marmotta-backend
 
+# create volume for settings (optional)
+# bx ic volume-create marmotta-settings
+
 # run postgres container
-bx ic run --name marmotta-db -e "POSTGRES_PASSWORD=changeme" -e "POSTGRES_USER=root" -e "POSTGRES_DB=marmotta" -p 5432:5432  -m 256 registry.eu-gb.bluemix.net/semantic_mediator_container/postgre
+bx ic run --name marmotta-db \
+	-e "POSTGRES_PASSWORD=changeme"\
+	-e "POSTGRES_USER=root" \
+	-e "POSTGRES_DB=marmotta" \
+	-p 5432:5432 \
+	-m 256 registry.eu-gb.bluemix.net/semantic_mediator_container/postgre
 
 # run marmotta
-bx ic run --name marmotta-backend -v /var/lib/marmotta --link marmotta-db:marmotta-db -p 8080:8080 -m 2048 registry.eu-gb.bluemix.net/semantic_mediator_container/marmotta-backend
+bx ic run --name marmotta-backend \
+	-v /var/lib/marmotta \
+	--link marmotta-db:marmotta-db \
+	-p 8080:8080 \
+	-v marmotta-settings:/var/lib/marmotta \
+	-m 2048 registry.eu-gb.bluemix.net/semantic_mediator_container/marmotta-backend
