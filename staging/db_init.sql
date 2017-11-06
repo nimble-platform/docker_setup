243715ffa4449297ad04284f056f9dc3e42525e1
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-11-06 15:21:30 +0100

    refactored file structure

---
+++ staging/db_init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE identitydb WITH OWNER admin;
+CREATE DATABASE catalogcategorydb WITH OWNER admin;
+CREATE DATABASE camundadb WITH OWNER admin;
+CREATE DATABASE businessprocessdb WITH OWNER admin;
+CREATE DATABASE ubldb WITH OWNER admin;
+CREATE DATABASE modalmldb WITH OWNER admin;
