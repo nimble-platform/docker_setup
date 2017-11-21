ddec761be3d2e6f2d76b9e5c073ba241c5a6409c
Johannes Innerbichler <j.innerbichler@gmail.com>
2017-11-21 13:50:03 +0100

    started stagin env

--- staging/db_init.sql
+++ staging/db_init.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE identitydb WITH OWNER admin;
+CREATE DATABASE keycloak WITH OWNER admin;
 CREATE DATABASE catalogcategorydb WITH OWNER admin;
 CREATE DATABASE camundadb WITH OWNER admin;
 CREATE DATABASE businessprocessdb WITH OWNER admin;
 CREATE DATABASE ubldb WITH OWNER admin;
 CREATE DATABASE modalmldb WITH OWNER admin;
+CREATE DATABASE catalogsyncdb WITH OWNER admin;
