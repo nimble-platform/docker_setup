68d7463595c3d4768960831521ea905ac7fa8341
Johannes Innerbichler <j.innerbichler@gmail.com>
2018-09-13 13:15:20 +0200

    Added second sync database to staging environment in order to avoid access from a ghost catalogue service.

--- staging/db_init.sql
+++ staging/db_init.sql
@@ -7,4 +7,5 @@ CREATE DATABASE businessprocessdb WITH OWNER admin;
 CREATE DATABASE ubldb WITH OWNER admin;
 CREATE DATABASE modalmldb WITH OWNER admin;
 CREATE DATABASE catalogsyncdb WITH OWNER admin;
+CREATE DATABASE catalogsyncdbstaging WITH OWNER admin;
 CREATE DATABASE datachanneldb WITH OWNER admin;
