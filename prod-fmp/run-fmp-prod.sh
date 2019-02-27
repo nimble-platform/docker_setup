56111c8af6cbd2caca51f3c771ab2406da4f07fb
Johannes Innerbichler <j.innerbichler@gmail.com>
2019-02-27 16:31:08 +0100

    Added database to FMP production instance setup.

--- prod-fmp/run-fmp-prod.sh
+++ prod-fmp/run-fmp-prod.sh
@@ -8,6 +8,10 @@ if [[ "$1" = "infra" ]]; then
 
 	docker-compose -f infra/docker-compose-prod-fmp.yml --project-name ${INFRA_PROJECT} up --build -d
 
+elif [[ "$1" = "database" ]]; then
+
+	docker-compose -f infra/docker-compose-prod-fmp.yml --project-name ${INFRA_PROJECT} up -d fmp-main-db
+
 elif [[ "$1" = "keycloak" ]]; then
 
 	docker-compose -f infra/keycloak/docker-compose-prod-fmp.yml --project-name ${INFRA_PROJECT} up --build -d
